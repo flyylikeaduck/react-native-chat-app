f53bb912751009d7b473be6c61c258bab68c1bdf
Rebecca Chen <flyylikeaduck@gmail.com>
2017-12-21 23:53:30 -0800

    Get and render messages

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import {send, subscribe} from 'react-native-training-chat-server';
 
 const NAME = 'Becca';
-const CHANNEL = 'Humma';
+const CHANNEL = 'Reactivate';
 
 export default class App extends React.Component {
+  state = {messages: []};
+
+  componentDidMount() {
+    subscribe(CHANNEL, messages => {
+      this.setState({messages});
+    });
+  }
+
+  renderItem({item}) {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.sender}>{item.sender}</Text>
+        <Text style={styles.message}>{item.message}</Text>
+      </View>
+    );
+  }
+
   render() {
+    const {messages} = this.state;
+
     return (
       <View style={styles.container}>
-         <Text>Humma! Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+         <FlatList data={messages} renderItem={this.renderItem} />
       </View>
     );
   }
